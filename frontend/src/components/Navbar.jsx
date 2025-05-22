a5e91d24d815d403350d0f510aaf6f3ecb5dde38
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-05-23 01:19:55 +0530

    Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaRegHeart, FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-4 shadow">
+      <div className="text-2xl">Scatch</div>
+      <div className="">
+        <ul className="flex gap-8  pr-4">
+          <Link>New Launches</Link>
+          <Link>Women</Link>
+          <Link>Men</Link>
+          <Link>Collection</Link>
+          <Link>Sale</Link>
+          <Link>Under 1999</Link>
+          <Link>Combo Sets</Link>
+          <Link>Summer Essential</Link>
+          <Link>
+            <FaUser />
+          </Link>
+          <Link>
+            <FaSearch />
+          </Link>
+          <Link>
+            <FaRegHeart />
+          </Link>
+          <Link>
+            <FaShoppingCart />
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
