7c1c740f7ab19c73c1cd2b6203b123add47699e9
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-05-23 21:08:15 +0530

    Login feature

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [message,setMessage]=useState("");
+
+  const handleOnChange = (e)=>{
+    setFormData((prev)=>({
+      ...prev,
+      [e.target.name]:e.target.value,
+    }))
+  }
+
+  const handleSubmit =async (e)=>{
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/users/login",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(formData),
+      })
+
+           const contentType = response.headers.get("content-type");
+
+    if (!response.ok) {
+      let errorMessage = "Failed to register";
+      if (contentType && contentType.includes("application/json")) {
+        const errorData = await response.json();
+        errorMessage = errorData.message || errorMessage;
+      }
+      throw new Error(errorMessage);
+    }
+
+      await response.json();
+      setMessage("User Logged in");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <div className="w-90 h-120  shadow-2xl p-8 flex flex-col gap-4">
+        <h1 className="text-3xl text-center">Login</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-xs text-gray-500">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={handleOnChange}
+              className="shadow p-2 rounded-md"
+              placeholder="example@gmail.com"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-xs text-gray-500">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              onChange={handleOnChange}
+              className="shadow p-2 rounded-md"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button type="submit" className="bg-blue-500 p-2 rounded-md w-[100px] ml-[90px] text-white font-semibold">
+          Login
+        </button>
+        </form>
+         {message && (
+          <p className="text-green-500 text-center mt-2">{message}</p>
+        )}
+        
+        <div className="ml-[80px] mt-4">
+          New User?{" "}
+          <Link className="text-blue-500" to="/signup">
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
