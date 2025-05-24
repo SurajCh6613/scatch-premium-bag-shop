7bb1132fde084eb2e9bb1b71468429f115e879ac
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-05-24 11:31:23 +0530

    dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Hero from "./pages/Hero";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Hero />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard"  element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
