a9bdb155a2f140a1862d5f1be9f11c628b8e10c8
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-05-24 11:31:42 +0530

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,52 +1,54 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const navigate = useNavigate()
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
-  const [message,setMessage]=useState("");
+  const [message, setMessage] = useState("");
 
-  const handleOnChange = (e)=>{
-    setFormData((prev)=>({
+  const handleOnChange = (e) => {
+    setFormData((prev) => ({
       ...prev,
-      [e.target.name]:e.target.value,
-    }))
-  }
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-  const handleSubmit =async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/users/login",{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"
+      const response = await fetch("http://localhost:5000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData),
-      })
+        credentials:"include",  // recieve token 
+        body: JSON.stringify(formData),
+      });
 
-           const contentType = response.headers.get("content-type");
+      const contentType = response.headers.get("content-type");
 
-    if (!response.ok) {
-      let errorMessage = "Failed to register";
-      if (contentType && contentType.includes("application/json")) {
-        const errorData = await response.json();
-        errorMessage = errorData.message || errorMessage;
+      if (!response.ok) {
+        let errorMessage = "Failed to login";
+        if (contentType && contentType.includes("application/json")) {
+          const errorData = await response.json();
+          errorMessage = errorData.message || errorMessage;
+        }
+        throw new Error(errorMessage);
       }
-      throw new Error(errorMessage);
-    }
-
-      await response.json();
-      setMessage("User Logged in");
+      const data = await response.json();
+      setMessage("User Logged in successfully");
+      navigate("/dashboard")
     } catch (error) {
-      console.log(error);
+      setMessage(error.message)
     }
-  }
+  };
 
-  
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="w-90 h-120  shadow-2xl p-8 flex flex-col gap-4">
@@ -78,14 +80,17 @@ const Login = () => {
               required
             />
           </div>
-          <button type="submit" className="bg-blue-500 p-2 rounded-md w-[100px] ml-[90px] text-white font-semibold">
-          Login
-        </button>
+          <button
+            type="submit"
+            className="bg-blue-500 p-2 rounded-md w-[100px] ml-[90px] text-white font-semibold"
+          >
+            Login
+          </button>
         </form>
-         {message && (
+        {message && (
           <p className="text-green-500 text-center mt-2">{message}</p>
         )}
-        
+
         <div className="ml-[80px] mt-4">
           New User?{" "}
           <Link className="text-blue-500" to="/signup">
