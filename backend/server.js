b387a8c5f8b1a91319c3ab7ca6861d1c87cc8325
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-05-24 11:30:58 +0530

    cors setup updated

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
-require('dotenv').config({path:'./config/.env'});
-const express = require('express');
-const connectDB = require('./config/db');
-const cookieParser = require('cookie-parser')
-const userRoutes = require('./routes/userRoutes')
-const cors = require('cors');
+require("dotenv").config({ path: "./config/.env" });
+const express = require("express");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
 
-app.use(cors()) //  allow all origin
-// app.use(cors({origin:'http://localhost:5173'})) //  allow specific origin 5173
-app.use(express.json()) // Middleware to parse json
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173", // your frontend origin
+    credentials: true, // allow cookies and credentials
+  })
+);
+app.use(express.json()); // Middleware to parse json
+app.use(cookieParser());
 
-app.use('/api/users',userRoutes)
+app.use("/api/users", userRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("Hello user");
-})
+app.get("/", (req, res) => {
+  res.send("Hello user");
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server running at PORT : ${process.env.PORT}`);  
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at PORT : ${process.env.PORT}`);
+});
